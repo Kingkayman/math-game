14884857e82757d44dab4f3ed9d9db6d6e34fdcd
Simon Nyberg Kassa <simonnyberg@live.se>
2026-05-23 14:46:55 +0200

    fix render deployment and update app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, session, redirect
 
 from game_logic import (
@@ -84,5 +85,7 @@ def index():
     )
 
 
+# 🚀 IMPORTANT FOR RENDER
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
